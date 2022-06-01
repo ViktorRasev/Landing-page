22abbbe6025fe79daccb3f635f19a96ec01d8c2c
Viktor Rasev <vrasev@gmail.com>
2022-06-01 23:52:48 +0200

    hamburger icon added

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// const aboutBc  = document.querySelector(".about")
-
-// const windowWidth = window.innerWidth / 2
-// const windowHeight = window.innerHeight / 2
 
 
-// aboutBc.addEventListener("mousemove", (e) => { 
-//     const mouseX = e.clientX / windowWidth
-//     const mouseY = e.clientY / windowHeight
-
-//     aboutBc.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`
-// })
+// Hamburger Icon  
+const menuBtn = document.querySelector(".menu-btn")
+let menuOpen = false
+menuBtn.addEventListener("click", () => { 
+    if(!menuOpen) { 
+        menuBtn.classList.add("open")
+        menuOpen = true
+    } else { 
+        menuBtn.classList.remove("open")
+        menuOpen = false
+    }
+})
