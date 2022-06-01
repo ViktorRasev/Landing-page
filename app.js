f42de8ece1cd5301d6436211d153b8680ef372d3
Viktor Rasev <vrasev@gmail.com>
2022-06-01 16:52:48 +0200

    added

---
+++ app.js
@@ -0,0 +1,12 @@
+// const aboutBc  = document.querySelector(".about")
+
+// const windowWidth = window.innerWidth / 2
+// const windowHeight = window.innerHeight / 2
+
+
+// aboutBc.addEventListener("mousemove", (e) => { 
+//     const mouseX = e.clientX / windowWidth
+//     const mouseY = e.clientY / windowHeight
+
+//     aboutBc.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`
+// })
