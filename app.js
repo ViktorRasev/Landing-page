e569911e0f4cdc7a2dbe7d40db9c2b182791ed15
Viktor Rasev <vrasev@gmail.com>
2022-06-02 02:48:29 +0200

    header hamburger menu functionality done

--- app.js
+++ app.js
@@ -2,13 +2,50 @@
 
 // Hamburger Icon  
 const menuBtn = document.querySelector(".menu-btn")
-let menuOpen = false
+const navMenu = document.querySelector(".nav-menu")
+
+const navLinks = document.querySelectorAll(".nav-link")
+// const aboutBtn = document.querySelector(".about-link")
+// const portfolioBtn = document.querySelector(".portfolio-link")
+// const skillsBtn = document.querySelector(".skills-link")
+// const contactBtn = document.querySelector(".contact-link")
+
+
 menuBtn.addEventListener("click", () => { 
-    if(!menuOpen) { 
-        menuBtn.classList.add("open")
-        menuOpen = true
-    } else { 
-        menuBtn.classList.remove("open")
-        menuOpen = false
-    }
+    menuBtn.classList.toggle("active")
+    navMenu.classList.toggle("active")
 })
+
+
+navLinks.forEach((link) => { 
+    link.addEventListener("click", () => { 
+        menuBtn.classList.remove("active")
+        navMenu.classList.remove("active")
+    })
+})
+
+
+
+
+
+
+
+
+
+
+// portfolioBtn.addEventListener("click", () => { 
+//     menuBtn.classList.remove("active")
+//     navMenu.classList.remove("active")
+// })
+// skillsBtn.addEventListener("click", () => { 
+//     menuBtn.classList.remove("active")
+//     navMenu.classList.remove("active")
+// })
+// aboutBtn.addEventListener("click", () => { 
+//     menuBtn.classList.remove("active")
+//     navMenu.classList.remove("active")
+// })
+// contactBtn.addEventListener("click", () => { 
+//     menuBtn.classList.remove("active")
+//     navMenu.classList.remove("active")
+// })
